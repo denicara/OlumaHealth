f99b73c9d4d064d91ea092c68893be3535ce3826
denicara <denicara@oakland.edu>
2025-06-04 17:05:48 -0400

    Added mobile support *WIP*

--- javor.js
+++ javor.js
@@ -1,1 +1,12 @@
 console.log("hiiiii");
+
+document.addEventListener("DOMContentLoaded", function(){
+    const menu = document.querySelector('#mobile-menu');
+    const menuLinks = document.querySelector('.nav-menu');
+
+    menu.addEventListener('click', function() {
+        menu.classList.toggle('is-active');
+        menuLinks.classList.toggle('active');
+    });
+});
+
